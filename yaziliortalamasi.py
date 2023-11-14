9b00a3162550f7521f22774afaf325a6bfe08946
Samet DER <dersamet4@gmail.com>
2023-11-14 14:40:22 +0300

    Hatalar giderildi.

--- yaziliortalamasi.py
+++ yaziliortalamasi.py
@@ -1,23 +1,26 @@
-vize = int(input("Vize sinav notunu giriniz(%30 Ortalama Etkisi): "))
-vize = (vize * 30 / 100)
-final = int(input("Final sinav notunu giriniz(%40 Ortalama Etkisi): "))
-if final < 40 :
-    print("Final notu 40'dan düşük bu sebeple kaldiniz! ")
-else:
-    final = (final * 40 / 100)
-final_uygulamasi = int(input("Final uygulama notunu giriniz(%30 Ortalama Etkisi): "))
-if final_uygulamasi < 40 :
-    print("Uygulama notu 40'dan düşük bu sebeple kaldiniz! ")
-else:
-    final_uygulamasi = (final_uygulamasi * 30 / 100)
-ortalama = final + final_uygulamasi + vize
-if ortalama > 85:
-    print(f'AA ile geçtiniz, tebrikler ve ortalamaniz: {ortalama}')
-elif ortalama <85 and ortalama >= 70:
-    print(f'BB ile geçtiniz, tebrikle ve ortlamaniz: {ortalama}')
-elif ortalama < 70 and ortalama >= 55:
-    print(f'CC ile geçtiniz, tebrikle ve ortlamaniz: {ortalama}')
-elif ortalama < 55 and ortalama >= 40:
-    print(f'DC ile geçtiniz, tebrikle ve ortlamaniz: {ortalama}')
-else:
-    print(f'Kaldiniz, ortalama: {ortalama}')
+yazili1 = float(input('1.Yazili Notunuzu Girin: '))
+yazili2 = float(input('2.Yazili Notunuzu Girin: '))
+sozlu = float(input('Sözlü: '))
+
+ortalama = (yazili1 + yazili2 + sozlu) / 3
+
+if  (ortalama >= 0) and (ortalama<25):
+    print(f'Ortalamaniz: {ortalama} notunuz: 0')
+
+elif  (ortalama >= 25) and (ortalama<44):
+    print(f'Ortalamaniz: {ortalama} notunuz: 1')
+
+elif  (ortalama >= 45) and (ortalama<55):
+    print(f'Ortalamaniz: {ortalama} notunuz: 2')
+    
+elif  (ortalama >= 55) and (ortalama<70):
+    print(f'Ortalamaniz: {ortalama} notunuz: 3')
+
+elif  (ortalama >= 70) and (ortalama<85):
+    print(f'Ortalamaniz: {ortalama} notunuz: 4')
+
+elif  (ortalama >= 85) and (ortalama<=100):
+    print(f'Ortalamaniz: {ortalama} notunuz: 5')
+
+else: 
+    print('Yanliş bilgi girdiniz! ')
